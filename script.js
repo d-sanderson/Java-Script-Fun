5d352d4a6a2efa7c7d7927c813aaefebd5787e8a
David Sanderson <dsanderson3@cnm.edu>
2018-10-16 08:13:45 -0600

    added utilCalc function

---
+++ script.js
@@ -0,0 +1,39 @@
+
+const person = {
+	age: 28,
+	name: 'David',
+	weight: 165,
+	eyes: blue,
+
+
+	greet() {
+		console.log('My name is ' + this.name)
+	},
+
+	mydeets() {
+		console.log('Hi, Im' + this.name +'!' + 'I weigh' + this.weight + ' lbs.' + 'my eyes are '+ this.eyes + ' and I am' + this.age + ' old')
+	}
+
+};
+
+
+
+function utilCalc(pnm, gas, water) {
+	this.pnm = pnm;
+	this.gas = gas;
+	this.water=water;
+	this.calc = function() {
+		result = (pnm + gas + water) / 3;
+		console.log(result)
+
+	}}
+
+
+
+
+//the value of const does not change if you don't need to reassign use const
+//if you need to reassign use let
+/*
+Primitives: Strings,
+
+ */
