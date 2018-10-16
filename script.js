4395b90e478fac259e021c9cfb4085c9a1f0a669
David Sanderson <dsanderson3@cnm.edu>
2018-10-16 08:40:27 -0600

    added utilCalc function

--- script.js
+++ script.js
@@ -3,27 +3,30 @@ const person = {
 	age: 28,
 	name: 'David',
 	weight: 165,
-	eyes: blue,
+	eyes: 'blue',
 
 
 	greet() {
 		console.log('My name is ' + this.name)
 	},
 
-	mydeets() {
-		console.log('Hi, Im' + this.name +'!' + 'I weigh' + this.weight + ' lbs.' + 'my eyes are '+ this.eyes + ' and I am' + this.age + ' old')
+	myDeets() {
+		console.log('Hi, I am ' + this.name +'!' + ' I weigh ' + this.weight + ' lbs.' + ' my eyes are '+ this.eyes + ' and I am ' + this.age + ' old')
 	},
 
 };
 
 
 
-function utilCalc(pnm, gas, water, internet) {
+function utilCalc(pnm, gas, water) {
+	internet = 32;
 	result = (pnm + gas + water +internet) / 3;
 	return result;
 }
 
-utilCalc();
+function greeter(name, age, eyecolor, weight) {
+	console.log('Hello ' + name + 'you have ' + eyecolor + 'eyes and weigh ' + weight + ' lbs.')
+}
 
 
 //the value of const does not change if you don't need to reassign use const
