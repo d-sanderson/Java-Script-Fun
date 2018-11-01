6c552a91f1e788c6fd419933a1198bcae6d91fa1
dsanderson90 <dsanderson3@cnm.edu>
2018-11-01 08:53:46 -0600

    added utilCalc function

--- snapchallenge102918.js
+++ snapchallenge102918.js
@@ -400,3 +400,18 @@ const filterMe = [{
 let filtered = filterMe.filter(array => {return array.userId !==6});
 
 console.log(filtered);
+
+
+/* Snap Challenge 110118 */
+
+
+const newArray = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+console.log(newArray.reduce(reducer));
+
+var sum = newArray.reduce( function(total, amount){
+	return total + amount
+});
+
+console.log(sum);
