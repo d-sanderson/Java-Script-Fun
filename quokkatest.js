7b79a7a1ec0d3bdb7d75dfe1d42c30c9d49f3736
dsanderson90 <dsanderson3@cnm.edu>
2018-12-29 15:48:40 -0700

    added pokemon if statement

--- quokkatest.js
+++ quokkatest.js
@@ -1,8 +1,13 @@
-const animals = ['cat', 'dog', 'parrot', 'cow', 'ferret', 'elephant', 'pikachu', 'snorlax', 'deer', 'lion', 'puma', 'pig', 'chicken', 'lamb', 'eel', 'goldfish'];
+let animals = ['cat', 'dog', 'parrot', 'cow', 'ferret', 'elephant', 'pikachu', 'snorlax', 'deer', 'lion', 'puma', 'pig', 'chicken', 'lamb', 'eel', 'goldfish'];
 
 
 for (var i = 0; i < animals.length; i++) {
-    animals[i] = i + 'is an animal';
+	if (animals[i] === 'pikachu' || animals[i] === 'snorlax')  {
+		animals[i] = animals[i] + ' is a pokemon!';
+	}
+	else {
+		animals[i] = animals[i] + ' is an animal';
+	}
 }
 
-console.log(animals)
+console.log(animals);
