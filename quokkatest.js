f3d72b28f975d03e210d42a38b4a949b3d49a3c7
dsanderson90 <dsanderson3@cnm.edu>
2018-12-29 09:30:08 -0700

    get in there quokka!

--- quokkatest.js
+++ quokkatest.js
@@ -1,1 +1,8 @@
-let animas = [cat, dog, parrot, cow, ferret, elephant, ]
+const animals = ['cat', 'dog', 'parrot', 'cow', 'ferret', 'elephant', 'pikachu', 'snorlax', 'deer', 'lion', 'puma', 'pig', 'chicken', 'lamb', 'eel', 'goldfish'];
+
+
+for (var i = 0; i < animals.length; i++) {
+    animals[i] = i + 'is an animal';
+}
+
+console.log(animals)
