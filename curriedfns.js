20889826628341a0ca598eecfc814c34f6a50325
dsanderson90 <dsanderson3@cnm.edu>
2019-08-30 16:35:00 -0600

    added comments.

--- curriedfns.js
+++ curriedfns.js
@@ -5,6 +5,9 @@ const createUrl = baseUrl => {
   };
 };
 
+//  This returns a function with the baseURL argument passed
 const createSiteUrl = createUrl('test.com');
+
+// You now use the createSiteUrl fn to set the path.
 const createSectionUrl = createSiteUrl('login')
 console.log(createSectionUrl);
